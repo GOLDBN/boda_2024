1b5a1758783f41283b1a7d2be70220613799d81a
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-04-06 00:02:33 -0600

    update form

--- resources/views/Wedding/index.blade.php
+++ resources/views/Wedding/index.blade.php
@@ -54,38 +54,37 @@
 		</div>
 	</header>
 
-	{{-- <div id="fh5co-couple">
+	 <div id="fh5co-couple">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
 					<h2>Hello!</h2>
-					<h3>November 28th, 2016 New York, USA</h3>
-					<p>We invited you to celebrate our wedding</p>
-				</div>
-			</div>
-			<div class="couple-wrap animate-box">
-				<div class="couple-half">
-					<div class="groom">
-						<img src="images/groom.jpg" alt="groom" class="img-responsive">
-					</div>
-					<div class="desc-groom">
-						<h3>Joefrey Mahusay</h3>
-						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
-					</div>
-				</div>
-				<p class="heart text-center"><i class="icon-heart2"></i></p>
-				<div class="couple-half">
-					<div class="bride">
-						<img src="images/bride.jpg" alt="groom" class="img-responsive">
-					</div>
-					<div class="desc-bride">
-						<h3>Sheila Mahusay</h3>
-						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
-					</div>
+					<form class="" action="">
+                        <form class="row g-3">
+
+                            <div class="col-12">
+                                <label for="inputAddress" class="form-label">Nombre completo</label>
+                                <input type="text" class="form-control" id="" placeholder="">
+                              </div>
+
+                            <div class="col-md-6" style="margin-top:2rem;">
+                              <label for="inputEmail4" class="form-label">Num de adultos</label>
+                              <input type="number" class="form-control" id="">
+                            </div>
+
+                            <div class="col-md-6" style="margin-top:2rem;">
+                              <label for="inputPassword4" class="form-label">Num de niño</label>
+                              <input type="number" class="form-control" id="">
+                            </div>
+
+                            <div class="col-12" >
+                              <button type="submit" class="btn btn-primary" style="margin-top:3rem;">Registrar</button>
+                            </div>
+                          </form>
 				</div>
 			</div>
 		</div>
-	</div> --}}
+	</div>
 
 	<div id="fh5co-event" class="fh5co-bg" style="background-image:url('{{ asset('wedding/images/img_bg_3.jpg')}}');">
 		<div class="overlay"></div>
@@ -172,7 +171,7 @@
 	<!-- Main -->
     <script src="{{ asset('wedding/js/main.js')}}"></script>
 	<script>
-    var d = new Date(new Date().getTime() + 200 * 120 * 120 * 2000);
+    var d = new Date(new Date().getTime() + 100 * 120 * 120 * 2000);
 
     // default example
     simplyCountdown('.simply-countdown-one', {
