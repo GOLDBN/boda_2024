7d5c25f9550fb3f9bc1f4f8f9a63ed5d64b07bab
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-04-17 12:11:51 -0600

    Actualzaicion de colores y fotos

--- resources/views/Wedding/index.blade.php
+++ resources/views/Wedding/index.blade.php
@@ -32,20 +32,21 @@
 	<!-- FOR IE9 below -->
 
 	</head>
-	<body>
+	<body >
 
 	<div class="fh5co-loader"></div>
 
 	<div id="page">
-	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url('{{ asset('wedding/images/img_bg_2 (1).jpg')}}');" data-stellar-background-ratio="0.5">
+	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url('{{ asset('wedding/images/4.jpg')}}');" data-stellar-background-ratio="0.5">
 		<div class="overlay"></div>
 		<div class="container">
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2 text-center">
 					<div class="display-t">
 						<div class="display-tc animate-box" data-animate-effect="fadeIn">
-							<h1>Joefrey &amp; Sheila</h1>
-							<h2>We Are Getting Married</h2>
+							<h1>Itzel &amp; Nacho</h1>
+                                Para nosotros lo más importante es tu presencia, pero si algo nos quieres obsequiar  con dinero lo sabremos apreciar
+                            </h2>
 							<div class="simply-countdown simply-countdown-one"></div>
 						</div>
 					</div>
@@ -58,7 +59,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
-					<h2>Hello!</h2>
+					<h2>Confirma  tu asistencia</h2>
 					<form class="" action="">
                         <form class="row g-3">
 
@@ -86,51 +87,38 @@
 		</div>
 	</div>
 
-	<div id="fh5co-event" class="fh5co-bg" style="background-image:url('{{ asset('wedding/images/img_bg_3.jpg')}}');">
+	<div id="fh5co-event" class="fh5co-bg" style="background-image:url('{{ asset('wedding/images/6.jpg')}}');">
 		<div class="overlay"></div>
 		<div class="container">
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
-					<span>Our Special Events</span>
-					<h2>Wedding Events</h2>
+					<h2>Detalles del evento</h2>
 				</div>
 			</div>
 			<div class="row">
 				<div class="display-t">
 					<div class="display-tc">
-						<div class="col-md-10 col-md-offset-1">
-							<div class="col-md-6 col-sm-6 text-center">
+						<div class="col-12 col-md-offset-1">
+							<div class="col-md-12 col-sm-6 text-center">
 								<div class="event-wrap animate-box">
-									<h3>Main Ceremony</h3>
+									<h3>Ceremonia y fiesta</h3>
 									<div class="event-col">
 										<i class="icon-clock"></i>
-										<span>4:00 PM</span>
+										<span><br></span>
 										<span>6:00 PM</span>
 									</div>
 									<div class="event-col">
 										<i class="icon-calendar"></i>
-										<span>Monday 28</span>
-										<span>November, 2016</span>
+										<span>Viernes 7</span>
+										<span>Junio, 2024</span>
 									</div>
-									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+									<a href="https://goo.gl/maps/Zs33vhPRxK6tp9KF6" target="_blank">
+                                      <strong>  Blvrd Barra Vieja, 39930 Acapulco de Juárez, Gro.</strong>
+                                    </a>
+
 								</div>
 							</div>
-							<div class="col-md-6 col-sm-6 text-center">
-								<div class="event-wrap animate-box">
-									<h3>Wedding Party</h3>
-									<div class="event-col">
-										<i class="icon-clock"></i>
-										<span>7:00 PM</span>
-										<span>12:00 AM</span>
-									</div>
-									<div class="event-col">
-										<i class="icon-calendar"></i>
-										<span>Monday 28</span>
-										<span>November, 2016</span>
-									</div>
-									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-								</div>
-							</div>
+
 						</div>
 					</div>
 				</div>
@@ -171,7 +159,7 @@
 	<!-- Main -->
     <script src="{{ asset('wedding/js/main.js')}}"></script>
 	<script>
-    var d = new Date(new Date().getTime() + 100 * 120 * 120 * 2000);
+    var d = new Date(new Date().getTime() + 419 * 120 * 120 * 2000);
 
     // default example
     simplyCountdown('.simply-countdown-one', {
